cd5e8d6a344e90aaf895cce8b717c2907895b8a9
guolejian <guolejian@126.com>
2019-02-01 10:28:00 +0800

    1.shiro

--- springbootlearn/src/main/java/com/guo/springboot/zookeeper/ZkUtil.java
+++ springbootlearn/src/main/java/com/guo/springboot/zookeeper/ZkUtil.java
@@ -7,17 +7,6 @@ import java.io.IOException;
 
 public class ZkUtil {
 
-    private static ZookeeperFactory zookeeperFactory = null;
-
-    static {
-        zookeeperFactory = ZookeeperFactory.getInstace();
-        try {
-            zookeeperFactory.init();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     private static class Holder {
         private static ZkUtil zkUtil = new ZkUtil();
     }
@@ -26,37 +15,44 @@ public class ZkUtil {
         return Holder.zkUtil;
     }
 
-    public String creteNode(String path, String content) {
+    public String creteNodePersistent(String path, String content) {
 
-        if (zookeeperFactory != null) {
+        /**
+         * CreateMode
+         * PERSISTENT  持久节点,
+         * PERSISTENT_SEQUENTIAL 持节顺序节点,
+         * EPHEMERAL  临时节点,
+         * EPHEMERAL_SEQUENTIAL 临时顺序节点;
+         */
+        return this.createNode(path, content, CreateMode.PERSISTENT);
+    }
 
-            /**
-             * CreateMode
-             * PERSISTENT  持久节点,
-             * PERSISTENT_SEQUENTIAL 持节顺序节点,
-             * EPHEMERAL  临时节点,
-             * EPHEMERAL_SEQUENTIAL 临时顺序节点;
-             */
-            return zookeeperFactory.createSync(path, content, CreateMode.PERSISTENT);
+    public String creteNodePersistentSeq(String path, String content) {
 
-        }
-        return null;
+        /**
+         * CreateMode
+         * PERSISTENT  持久节点,
+         * PERSISTENT_SEQUENTIAL 持节顺序节点,
+         * EPHEMERAL  临时节点,
+         * EPHEMERAL_SEQUENTIAL 临时顺序节点;
+         */
+        return this.createNode(path, content, CreateMode.PERSISTENT_SEQUENTIAL);
+
     }
 
     public String creteNodeEPHEMERAL(String path, String content) {
 
-        if (zookeeperFactory != null) {
+        /**
+         * CreateMode
+         * PERSISTENT  持久节点,
+         * PERSISTENT_SEQUENTIAL 持节顺序节点,
+         * EPHEMERAL  临时节点,
+         * EPHEMERAL_SEQUENTIAL 临时顺序节点;
+         */
+        return this.createNode(path, content, CreateMode.EPHEMERAL);
+    }
 
-            /**
-             * CreateMode
-             * PERSISTENT  持久节点,
-             * PERSISTENT_SEQUENTIAL 持节顺序节点,
-             * EPHEMERAL  临时节点,
-             * EPHEMERAL_SEQUENTIAL 临时顺序节点;
-             */
-            return zookeeperFactory.createSync(path, content, CreateMode.EPHEMERAL);
-
-        }
-        return null;
+    public String createNode(String path, String content, CreateMode nodeMode) {
+       return ZookeeperFactory.getInstace().createSync(path, content, nodeMode);
     }
 }
--- springbootlearn/src/main/java/com/guo/springboot/zookeeper/ZookeeperFactory.java
+++ springbootlearn/src/main/java/com/guo/springboot/zookeeper/ZookeeperFactory.java
@@ -19,36 +19,44 @@ public class ZookeeperFactory implements Watcher{
         return Holder.INSTANCE;
     }
 
-    public void init() throws IOException {
+    private ZooKeeper getZookeeperClient() {
 
         if (zooKeeper == null) {
             synchronized (this) {
                 if (zooKeeper == null) {
+
+                    try {
 //                    zooKeeper = new ZooKeeper("127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183",
 //                            5000, new ZookeeperFactory());
-                    zooKeeper = new ZooKeeper("47.99.145.78:2181,47.99.145.78:2182,47.99.145.78:2183",
-                            5000, new ZookeeperFactory());
-                    try {
+                        zooKeeper = new ZooKeeper("47.99.145.78:2181,47.99.145.78:2182,47.99.145.78:2183",
+                                5000, new ZookeeperFactory());
+
                         connectSemphore.await();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+
                 }
             }
         }
+        return zooKeeper;
     }
 
     @Override
     public void process(WatchedEvent watchedEvent) {
         if (Watcher.Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            System.out.println("监听到了-----------------");
             connectSemphore.countDown();
         }
     }
 
     public String createSync(String path, String content, CreateMode createMode) {
         try {
-            String nodeName = zooKeeper.create(path, content.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, createMode);
-            return nodeName;
+            if (this.getZookeeperClient() != null) {
+                return this.getZookeeperClient().create(path, content.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, createMode);
+            }
         } catch (KeeperException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
