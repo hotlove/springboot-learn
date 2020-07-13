f07ce7ae3fc2dc92b0271eb1fff48091c98c13a5
guolejian <guolejian@126.com>
2020-07-13 17:05:01 +0800

    netty

--- springbootlearn/src/main/java/com/guo/springboot/netty/websocket/WebSocketFrameHandler.java
+++ springbootlearn/src/main/java/com/guo/springboot/netty/websocket/WebSocketFrameHandler.java
@@ -3,6 +3,8 @@ package com.guo.springboot.netty.websocket;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 
@@ -13,16 +15,21 @@ import java.util.Locale;
  * @Date: 2020/7/11 23:55
  * @Description:
  */
-@ChannelHandler.Sharable
 public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
         if (frame instanceof TextWebSocketFrame) {
+            System.out.println("接收消息。。。。。。。。。。。。。");
             // Send the uppercase string back.
             String request = ((TextWebSocketFrame) frame).text();
             ctx.channel().writeAndFlush(new TextWebSocketFrame(request.toUpperCase(Locale.US)));
+        } if (frame instanceof PingWebSocketFrame) {
+            System.out.println("ping/pong msg...................");
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+            return;
         } else {
             String message = "unsupported frame type: " + frame.getClass().getName();
+            System.out.println(message);
             throw new UnsupportedOperationException(message);
         }
     }
@@ -34,6 +41,8 @@ public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocket
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
 //        super.handlerAdded(ctx);
+        System.out.println("客户端建立连接");
+        ctx.channel().writeAndFlush("连接完成");
     }
 
     @Override
