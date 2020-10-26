823e1ba1a031b688d1ce942ba6dd8207bc396efa
hotlove_linx <guolejian@126.com>
2020-10-26 20:45:39 +0800

    1.增加netty单聊

--- springbootlearn/src/main/java/com/guo/springboot/thread/ThreadTest.java
+++ springbootlearn/src/main/java/com/guo/springboot/thread/ThreadTest.java
@@ -20,7 +20,7 @@ public class ThreadTest {
                 try {
 //                    System.out.println("任务开始执行------");
                     System.out.println("线程:"+Thread.currentThread().getName()+"执行了超时任务");
-                    TimeUnit.SECONDS.sleep(3);
+                    TimeUnit.SECONDS.sleep(2);
                 } catch (InterruptedException e) {
 //                    e.printStackTrace();
                     System.out.println("线程:"+Thread.currentThread().getName()+"任务中断"+(System.currentTimeMillis() - sartTime));
