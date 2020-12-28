38b9a6a10f96109b945914b7421ad2b4e54310f5
guolejian <guolejian@126.com>
2020-12-28 17:45:11 +0800

    1.实现了线程池监控线程的执行时间

--- springbootlearn/src/main/java/com/guo/springboot/kafka/ConsumerFastStart.java
+++ springbootlearn/src/main/java/com/guo/springboot/kafka/ConsumerFastStart.java
@@ -3,6 +3,7 @@ package com.guo.springboot.kafka;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -22,13 +23,13 @@ public class ConsumerFastStart {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializezr");
+//        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializezr");
         properties.put("bootstrap.servers", brokerList);
         //设置消费组的名称，具体的释义可以参⻅第3章
         properties.put("group.id", groupId);
         //创建⼀个消费者客户端实例
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties, new StringDeserializer(), new StringDeserializer());
         //订阅主题
         consumer.subscribe(Collections.singletonList(topic));
         //循环消费消息
